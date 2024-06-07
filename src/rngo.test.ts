4f9368ce378573b0bca10fa377345eb83c469d93
Aoife Lee <aoife@rngo.dev>
2024-06-06 22:48:06 -0400

    Fix timebomb in tests

--- src/rngo.test.ts
+++ src/rngo.test.ts
@@ -2,9 +2,8 @@ import { jest } from '@jest/globals'
 
 import { Rngo } from '.'
 
-// TODO: build one that will never be expired
 const jwt =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gU21pdGgiLCJpYXQiOjE1MTYyMzkwMjIsImV4cCI6MTcxNjI0OTAyMn0.zUx8DHI4xzKPG9mbXMjKGJ0NGC9ZGebsLBggNVK1bs0'
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyLCJleHAiOjQ4NzEzMjgyODB9.zIfdnIeZjNN3WOJc2my-5Sm2BUOpaTKXJGxYcwQBydQ'
 
 async function validRngo() {
   const initResult = await Rngo.init({ apiToken: jwt })
